4112bc398def2670ec8a0aafe601fd88afec44d4
Michaeldam88 <michaeldam88@gmail.com>
2024-02-01 20:36:40 +0100

    updates spacer

--- ui/rangeInput/rangeInput.tsx
+++ ui/rangeInput/rangeInput.tsx
@@ -1,5 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
-//import "./rangeInput.scss";
+import { ReactNode, useEffect,  } from "react";
 import "./rangeInput.scss";
 
 const RangeInput = ({
